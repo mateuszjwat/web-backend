6994387ee425148d9a822d254e80c27a43b35ee7
mateusz jarzebski <mateusz.jarzebski01@student.wat.edu.pl>
2021-06-08 12:05:04 +0200

    test rep

--- src/main/java/pl/edu/wat/backend/entities/DemoEntity.java
+++ src/main/java/pl/edu/wat/backend/entities/DemoEntity.java
@@ -1,14 +1,10 @@
 package pl.edu.wat.backend.entities;
 
-import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Data
+@Table(name = "demo")
 public class DemoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
